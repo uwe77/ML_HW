17073990824ed2f633c01df2985b418f24f3a988
uwe <uwe90711@gmail.com>
2023-10-13 17:51:10 +0800

    aasdasdasdas

---
+++ hw2/data.py
@@ -0,0 +1,23 @@
+class data:
+    def __init__(self, y=0) -> None:
+        self._feature = []
+        self._label = int(y)
+
+    def __setitem__(self, index, value)->None:
+        self._feature[index] = value
+
+    def __getitem__(self, index)->None:
+        return self._feature[index]
+
+    def __len__(self):
+        return self._label
+
+    def append(self, x):
+        self._feature.append(float(x))
+
+    def __str__(self) -> str:
+        return f'{self._feature},{self._label}'
+
+    def set_label(self, y = 0):
+        self._label = int(y)
+        return self
--- hw2/main.py
+++ hw2/main.py
@@ -4,71 +4,46 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from itertools import combinations as C
 import math
-class data:
-    def __init__(self, y=0) -> None:
-        self._feature = []
-        self._label = int(y)
-
-    def __setitem__(self, index, value)->None:
-        self._feature[index] = value
-
-    def __getitem__(self, index)->None:
-        return self._feature[index]
-
-    def __len__(self):
-        return self._label
-
-    def append(self, x):
-        self._feature.append(float(x))
-
-    def __str__(self) -> str:
-        return f'{self._feature},{self._label}'
-
-    def set_label(self, y = 0):
-        self._label = int(y)
-        return self
-
+from data import data
 class LDA:
-    def __init__(self, x_train, y_train, cin1=1, cin2=1) -> None:
-        num_posi = 0
-        num_nage = 0
-        dim1x2 = (1, int(np.array(x_train).shape[1]))
-        m_posi = np.zeros(dim1x2)
-        m_nage = np.zeros(dim1x2)
-        self._c1 = cin1
-        self._c2 = cin2
+    def __init__(self, x_train, y_train, c1 = 1, c2 = 2) -> None:
+        self._n_features = len(x_train[0])
+        self._n_classes = len(np.unique(y_train))
+        mean_overall = np.mean(np.array(x_train), axis=0)
 
-        for i in range(len(x_train)):
-            if y_train[i]:
-                num_posi += 1
-                m_posi += np.array(x_train[i])
-            else:
-                num_nage += 1
-                m_nage += np.array(x_train[i])
-        m_posi /= num_posi
-        m_nage /= num_nage
-        p_posi = num_posi / (num_nage+ num_posi)
-        p_nage = num_nage / (num_nage+ num_posi)
-        dim2x2 = (np.array(x_train).shape[1], np.array(x_train).shape[1])
-        s_posi = np.zeros(dim2x2)
-        s_nage = np.zeros(dim2x2)
-        for i in range(num_nage+num_posi):
-            if y_train[i]:
-                temp_2x1 = np.array(x_train[i]) - m_posi
-                s_posi += np.matmul(temp_2x1, np.transpose(temp_2x1))
-            else:
-                temp_2x1 = np.array(x_train[i]) - m_nage
-                s_nage += np.matmul(temp_2x1, np.transpose(temp_2x1))
-        self._segama = s_nage*p_nage+ s_posi*p_posi
-        print(np.transpose(m_posi-m_nage).shape)
-        print(self._segama)
-        # print(np.linalg.inv(self._segama).shape)
-        # self._w_t = np.matmul(np.transpose(m_posi-m_nage),np.linalg.inv(self._segama))
-        # self._b = np.matmul(self._w_t, (m_posi+m_nage))/(-2) - math.log(self.c1*p_nage/(self._c2*p_posi))
+        SP = np.zeros((self._n_features, self._n_features))
+        SN = np.zeros((self._n_features, self._n_features))
 
-        # print(f'self._w_t = \n{self._w_t}\nself._b = \n{self._b}')
-        
+        for c in np.unique(y_train):
+            print(f'c = {c}')
+            x_c = np.array(x_train)[y_train == c]
+            print(f'x_c =\n{x_c}')
+            mean_c = np.mean(x_c, axis=0)
+            print(f'mean_c =\n{mean_c}')
+            SP += (x_c - mean_c).T.dot((x_c - mean_c))
+            print(f'SP =\n{SP}')
+            mean_diff = (mean_c - mean_overall).reshape(self._n_features, 1)
+            print(f'mean_diff =\n{mean_diff}')
+            SN += mean_diff.dot(mean_diff.T)
+            print(f'SN =\n{SN}')
+        print(f'SP =\n{SP}')
+        print(f'SN =\n{SN}')
+        eigenvalues, eigenvectors = np.linalg.eig(np.linalg.inv(SP).dot(SN))
+        print(f'eigenvalues =\n{eigenvalues}')
+        eigenvectors = eigenvectors.T
+        print(f'eigenvectors =\n{eigenvectors}')
+        idxs = np.argsort(abs(eigenvalues))[::-1]
+        print(f'idxs =\n{idxs}')
+        eigenvalues = eigenvalues[idxs]
+        print(f'eigenvalues =\n{eigenvalues}')
+        eigenvectors = eigenvectors[idxs]
+        print(f'eigenvectors =\n{eigenvectors}')
 
+        self._linear_discriminants = eigenvectors[:self._n_classes - 1]
+        print(f'self._linear_discriminants =\n{self._linear_discriminants}')
+
+    def transform(self, x):
+        return np.dot(x, self._linear_discriminants.T)
 
 datas_label1 = []
 datas_label2 = []
@@ -95,4 +70,8 @@ for i in range(25,50):
     test_datas.append(datas_label2[i].set_label(1))
     test_datas.append(datas_label3[i].set_label(0))
 
-D = LDA([i[2:] for i in train_datas], [len(i) for i in train_datas], 1, 1)
+D_test = LDA(np.array([[1, 1],
+                       [3, 1],
+                       [4, 4],
+                       [8, 6]]), np.array([0, 0, 1, 1]))
+D = LDA(np.array([i[2:] for i in train_datas]), np.array([len(i) for i in train_datas]))
