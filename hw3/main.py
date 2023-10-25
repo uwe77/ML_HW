5c804cb4cc3b33e4b018a52ba1d58a6f81fc9165
uwe_home <uwe90711@gmail.com>
2023-10-26 00:37:58 +0800

    asda

--- hw3/main.py
+++ hw3/main.py
@@ -15,17 +15,17 @@ class SVM:
         # Set up the quadratic programming problem
         P = np.zeros((num_features + 1, num_features + 1)) # P is the matrix of the quadratic objective function
         P[:num_features, :num_features] = np.eye(num_features) # P is the identity matrix
-        print(P)
+        print("P=\n",P)
         q = np.zeros(num_features + 1) # q is the vector of the quadratic objective function
         q[-1] = self.C # q is the penalty parameter of the error term
-        print(q)
+        print("q=\n",q)
         G = -np.diag(y) @ np.hstack((X, np.ones((num_samples, 1)))) # G is the matrix of the inequality constraints
-        print(G)
+        print("G=\n",G)
         h = -np.ones(num_samples) # h is the vector of the inequality constraints
-        print(h)
+        print("h=\n",h)
         # Solve the quadratic programming problem
         alpha = solve_qp(P, q, G, h) # alpha is the Lagrange multipliers
-        print(alpha)
+        print("alpha=\n",alpha)
         # Calculate the weights and bias
         self.w = X.T @ (alpha[:-1] * y) # w is the weights
         print(self.w)
@@ -59,9 +59,9 @@ for line in f.readlines():
 # 将数据转换为NumPy数组
 x_test = np.vstack((np.array([i[2:] for i in datas3[25:]]), np.array([i[2:] for i in datas2[25:]])))
 x_train = np.vstack((np.array([i[2:] for i in datas3[:25]]), np.array([i[2:] for i in datas2[:25]])))
-y_train = np.hstack((np.array([len(i) for i in datas3[:25]]), np.array([len(i) for i in datas2[:25]])))
+y_train = np.hstack((np.array([-1 for i in datas3[:25]]), np.array([1 for i in datas2[:25]])))
 y_ans = np.hstack((np.array([len(i) for i in datas3[25:]]), np.array([len(i) for i in datas2[25:]])))
-X = x_train.T
+X = x_train
 y = y_train
 
 # 将数据转换为特征矩阵和标签向量
