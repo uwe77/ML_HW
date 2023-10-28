0bc09f84b8edd69cd294d217f786f165b84b89f5
uwe <uwe90711@google.com>
2023-10-28 20:01:27 +0800

    asdasd

--- hw3/SVM.py
+++ hw3/SVM.py
@@ -2,18 +2,19 @@ import numpy as np
 from qpsolvers import solve_qp, solve_problem, Problem
 
 #=========================================Linear_SVM==========================================================
-def linear_kernel(x1, x2):
-    return np.dot(x1, x2.T)
-
 class Linear_SVM:
     def __init__(self, C=1.0):
         self.C = C
+        self.alpha = None
         self.w = None
         self.b = None
 
+    def kernel(self, x1, x2):
+        return np.dot(x1, x2.T)
+
     def fit(self, X, y):
         n_samples, n_features = X.shape
-        K = linear_kernel(X, X)
+        K = self.kernel(X, X)
         P = np.outer(y, y) * K
         q = -np.ones((n_samples, 1))
         A = y.astype(np.double)
@@ -23,49 +24,46 @@ class Linear_SVM:
         problem = Problem(P, q, None, None, A, b, lb, ub)
         solution = solve_problem(problem, solver='proxqp')
         alpha = np.array([round(i, 4) for i in solution.x])
+        self.alpha = alpha
         self.w = np.array([round(i, 4) for i in np.dot(X.T, alpha * y)])
         tmp_x = np.array(X)[np.logical_and(0 < alpha, alpha < self.C), :]
         tmp_y = np.array(y)[np.logical_and(0 < alpha, alpha < self.C)]
         self.b = round(np.mean(tmp_y - np.dot(tmp_x, self.w)), 4)
-        print("alpha:\n", alpha, "\nand sum: ", round(sum(alpha), 4))
-        print("weight: ",self.w)
+        # print("alpha:\n", alpha, "\nand sum: ", round(sum(alpha), 4))
+        # print("weight: ",self.w)
         print("bias: ",self.b)
 
     def predict(self, X):
         linear_model = np.dot(X, self.w) + self.b
         return np.sign(linear_model)
 #=====================================RBF_SVM==============================================================    
-
-def RBF_kernel(x1, x2, sigama=1.0):
-    n_samples1, n_features1 = x1.shape
-    n_samples2, n_features2 = x2.shape
-    print(n_samples2)
-    K = np.zeros((n_samples1, n_samples2))
-    for i in range(n_samples1):
-        for j in range(n_samples2):
-            K[i, j] = np.exp(-np.linalg.norm(x1[i] - x2[j])**2 / (2 * sigama**2))
-    return K
 class RBF_SVM(Linear_SVM):
     def __init__(self, C=1.0, sigama=1.0):
         self.sigama = sigama
         super().__init__(C)
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        K = RBF_kernel(X, X, self.sigama)
-        P = np.outer(y, y) * K
-        q = -np.ones((n_samples, 1))
-        A = y.astype(np.double)
-        b = np.array([0.0])
-        lb = np.zeros(n_samples).astype(np.double)
-        ub = np.ones(n_samples)* self.C
-        problem = Problem(P, q, None, None, A, b, lb, ub)
-        solution = solve_problem(problem, solver='proxqp')
-        alpha = np.array([round(i, 4) for i in solution.x])
-        self.w = np.array([round(i, 4) for i in np.dot(X.T, alpha * y)])
-        tmp_x = np.array(X)[np.logical_and(0 < alpha, alpha < self.C), :]
-        tmp_y = np.array(y)[np.logical_and(0 < alpha, alpha < self.C)]
-        self.b = round(np.mean(tmp_y - np.dot(tmp_x, self.w)), 4)
-        print("alpha:\n", alpha, "\nand sum: ", round(sum(alpha), 4))
-        print("weight: ",self.w)
-        print("bias: ",self.b)
+    def kernel(self, x1, x2):
+        n_samples1, n_features1 = x1.shape
+        n_samples2, n_features2 = x2.shape
+        K = np.zeros((n_samples1, n_samples2))
+        for i in range(n_samples1):
+            for j in range(n_samples2):
+                K[i, j] = np.exp(-np.linalg.norm(x1[i] - x2[j])**2 / (2 * self.sigama**2))
+        return K
+#=====================================poly_SVM==============================================================
+class poly_SVM(Linear_SVM):
+    def __init__(self, C, p) -> None:
+        self.p = p
+        super().__init__(C)
+    
+    def kernel(self, x1, x2):
+        K = (np.dot(x1, x2.T)) ** self.p
+        return K
+    
+    # def fit(self, X, y):
+    #     super().fit(X, y)
+    #     self.y_train = y
+
+    # def predict(self, X):
+    #     K = self.kernel(X, X)
+    #     return np.sign(np.dot(K, self.alpha * self.y_train) + self.b)
--- hw3/main.py
+++ hw3/main.py
@@ -1,49 +1,13 @@
 from data import data
 import re
 import numpy as np
-from SVM import Linear_SVM, RBF_SVM
+from SVM import Linear_SVM, RBF_SVM, poly_SVM
 from qpsolvers import solve_qp, solve_problem, Problem
 
 
 def score(y_true, y_pred):
     score_board = np.array([1 if y_true[i] == y_pred[i] else 0 for i in range(len(y_true))])
     return np.mean(score_board)*100
-    # pass
-
-def poly_kernel(x1, x2, p=1):
-    print(((np.dot(x1, x2.T) + 1) ** p).shape)
-    return (np.dot(x1, x2.T) + 1) ** p
-class poly_SVM(Linear_SVM):
-    def __init__(self, C, p) -> None:
-        self.p = p
-        super().__init__(C)
-    
-    def fit(self, X, y):
-        self.y_train = y
-        n_samples, n_features = X.shape
-        K = poly_kernel(X, X, self.p)
-        n_samples = K.shape[0]
-        P = np.outer(y, y) * K
-        q = -np.ones((n_samples, 1))
-        A = y.astype(np.double)
-        b = np.array([0.0])
-        lb = np.zeros(n_samples).astype(np.double)
-        ub = np.ones(n_samples)* self.C
-        problem = Problem(P, q, None, None, A, b, lb, ub)
-        solution = solve_problem(problem, solver='proxqp')
-        self.alpha = np.array([round(i, 4) for i in solution.x])
-        self.w = np.array([round(i, 4) for i in np.dot(X.T, self.alpha * y)])
-        tmp_x = np.array(X)[np.logical_and(0 < self.alpha, self.alpha < self.C), :]
-        tmp_y = np.array(y)[np.logical_and(0 < self.alpha, self.alpha < self.C)]
-        self.b = round(np.mean(tmp_y - np.dot(tmp_x, self.w)), 4)
-        print("alpha:\n", self.alpha, "\nand len: ", len(self.alpha), "sum: ", round(sum(self.alpha), 4))
-        print("weight: ",self.w)
-        print("bias: ",self.b)
-
-    def predict(self, X):
-        K = poly_kernel(X, X, self.p)
-        return np.sign(np.dot(K, self.alpha * self.y_train) + self.b)
-
 
 datas1 = []
 datas2 = []
@@ -61,18 +25,13 @@ for line in f.readlines():
     elif len(data_input) == 3:
         datas3.append(data_input)
 
-# 将数据转换为NumPy数组
+# 將數據轉換為numpy array
 x_test = np.vstack((np.array([i[2:] for i in datas2[25:]]), np.array([i[2:] for i in datas3[25:]])))
 x_train = np.vstack((np.array([i[2:] for i in datas2[:25]]), np.array([i[2:] for i in datas3[:25]])))
 y_train = np.hstack((np.array([1 for i in datas2[:25]]), np.array([-1 for i in datas3[:25]])))
 y_ans = np.hstack((np.array([1 for i in datas2[25:]]), np.array([-1 for i in datas3[25:]])))
 
-svm = poly_SVM(C=10, p=2)
-svm.fit(x_train, y_train)
-svm_ans = svm.predict(x_test)
-print("score: ", score(y_ans, svm_ans))
-
-# #=========================================Linear_SVM==========================================================
+# =========================================Linear_SVM==========================================================
 # c_rate = np.array([])
 # for i in [1, 10, 100]:
 #     svm = Linear_SVM(C=i)
@@ -82,24 +41,64 @@ print("score: ", score(y_ans, svm_ans))
 #     print("score_C", i, "_1: ", score(y_ans, svm_ans))
 #     svm.fit(x_test, y_ans)
 #     svm_ans = svm.predict(x_train)
-#     c_rate = np.append(c_rate, score(y_ans, svm_ans))
-#     print("score_C", i, "_2: ", score(y_ans, svm_ans))
+#     c_rate = np.append(c_rate, score(y_train, svm_ans))
+#     print("score_C", i, "_2: ", score(y_train, svm_ans))
 # c_rate = np.reshape(c_rate, (3, 2))
 # c_rate = np.array([round(np.mean(c_rate[i]), 2) for i in range(3)])
 # print("c_rate: ", c_rate)
 # #=========================================RBF_SVM=============================================================
 # c_rate = np.array([])
+# svm = RBF_SVM(C=10, sigama=5)
+# svm.fit(x_train, y_train)
+# svm_ans = svm.predict(x_test)
+# c_rate = np.append(c_rate, score(y_ans, svm_ans))
+# print("score_C10_p", 1, "_1: ", score(y_ans, svm_ans))
+# svm.fit(x_test, y_ans)
+# svm_ans = svm.predict(x_train)
+# c_rate = np.append(c_rate, score(y_train, svm_ans))
+# print("score_C10_p", 1, "_2: ", score(y_train, svm_ans))
+# print("c_rate: ", c_rate)
+
+c_rate = np.array([])
+for i in [1, 10, 100]:
+    for j in [1, 0.5, 0.1, 0.05]:
+        svm = RBF_SVM(C=i, sigama=j)
+        svm.fit(x_train, y_train)
+        svm_ans = svm.predict(x_test)
+        c_rate = np.append(c_rate, score(y_ans, svm_ans))
+        print("score_C", i, "_sigama", j, ": ", score(y_ans, svm_ans))
+        svm.fit(x_test, y_ans)
+        svm_ans = svm.predict(x_train)
+        c_rate = np.append(c_rate, score(y_train, svm_ans))
+        print("score_C", i, "_sigama", j, ": ", score(y_train, svm_ans))
+c_rate = np.reshape(c_rate, (12, 2))
+c_rate = np.array([round(np.mean(c_rate[i]), 2) for i in range(12)])
+print("c_rate: ", c_rate)
+#=========================================poly_SVM============================================================
+# c_rate = np.array([])
+# svm = poly_SVM(C=10, p=1)
+# svm.fit(x_train, y_train)
+# svm_ans = svm.predict(x_test)
+# c_rate = np.append(c_rate, score(y_ans, svm_ans))
+# print("score_C10_p", 1, "_1: ", score(y_ans, svm_ans))
+# svm.fit(x_test, y_ans)
+# svm_ans = svm.predict(x_train)
+# c_rate = np.append(c_rate, score(y_train, svm_ans))
+# print("score_C10_p", 1, "_2: ", score(y_train, svm_ans))
+# print("c_rate: ", c_rate)
+
+# c_rate = np.array([])
 # for i in [1, 10, 100]:
-#     for j in [1, 0.5, 0.1, 0.05]:
-#         svm = RBF_SVM(C=i, sigama=j)
+#     for j in [2, 3, 4, 5]:
+#         svm = poly_SVM(C=i, p=j)
 #         svm.fit(x_train, y_train)
 #         svm_ans = svm.predict(x_test)
 #         c_rate = np.append(c_rate, score(y_ans, svm_ans))
-#         print("score_C", i, "_sigama", j, ": ", score(y_ans, svm_ans))
+#         print("score_C", i, "_p", j, ": ", score(y_ans, svm_ans))
 #         svm.fit(x_test, y_ans)
 #         svm_ans = svm.predict(x_train)
-#         c_rate = np.append(c_rate, score(y_ans, svm_ans))
-#         print("score_C", i, "_sigama", j, ": ", score(y_ans, svm_ans))
+#         c_rate = np.append(c_rate, score(y_train, svm_ans))
+#         print("score_C", i, "_p", j, ": ", score(y_train, svm_ans))
 # c_rate = np.reshape(c_rate, (12, 2))
 # c_rate = np.array([round(np.mean(c_rate[i]), 2) for i in range(12)])
 # print("c_rate: ", c_rate)
